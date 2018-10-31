f87846ce3dd6506c6ea2c1cb249228d3f6ed4f94
Daniel Diele <daniel.diele@stud.hs-hannover.de>
2018-10-31 12:09:32 +0100

    Stable version 1;

--- src/de/hsh/dbs/Main.java
+++ src/de/hsh/dbs/Main.java
@@ -1,6 +1,5 @@
 package de.hsh.dbs;
 
-import javax.xml.transform.Result;
 import java.sql.*;
 
 public class Main {
@@ -8,20 +7,20 @@ public class Main {
 
 
     public static void main(String args[]) throws SQLException {
-        String sql = null;
+        String query = "SELECT * FROM Movie";
         Statement stmt = null;
         ResultSet rs = null;
-        Connection con = null;
+        //Connection con;
 
         try {
-            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:db01", u.getUsername(), u.getPassword());
-            sql = "SELECT *" + " FROM Movie";
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:db01", u.getUsername(), u.getPassword());
             stmt = con.createStatement();
-            rs = stmt.executeQuery(sql);
+            rs = stmt.executeQuery(query);
+            System.out.println("Executing query....");
 
-                while( rs.next() ){
-                    String title = rs.getString("title");
-                    System.out.println("Movie: " + title);
+                while(rs.next()){
+                    String title= rs.getString("title");
+                    System.out.println("Genre: " + title);
                 }
         } catch ( SQLException var9) {
             var9.printStackTrace();
